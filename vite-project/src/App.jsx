cb049145d0cf8ee75be02c362701708393dce950
Nithishuchiha <124854933+Nithishuchiha@users.noreply.github.com>
2025-01-27 20:04:32 +0530

    about me component added

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,15 +1,16 @@
-import React from "react"
-import { Routes, BrowserRouter as Router, Route } from "react-router-dom"
-import HomePage from "./components/Home/HomePage"
-import Navbar from "./components/Navbar/Navbar"
+import { Routes, BrowserRouter as Router, Route } from "react-router-dom";
+import HomePage from "./components/Home/HomePage";
+import Navbar from "./components/Navbar/Navbar";
+import AboutMe from "./components/AboutUs/AboutUs";
 
 function App() {
-    <Router>
-        <Navbar />
-        <Routes>
-            <Route path="/" element={<HomePage />} />
-        </Routes>
-    </Router>
+  <Router>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/aboutus" element={<AboutMe />} />
+    </Routes>
+  </Router>;
 }
 
-export default App
+export default App;
---
+++ vite-project/src/components/AboutUs/AboutUs.jsx
@@ -0,0 +1,11 @@
+/**
+ * @author Nithish_kumar_P
+ * @description These page has a content oƒ about us page
+ * @returns {JSX.ELEMENT} These page returns about us component
+ */
+
+const AboutUs = () => {
+  return <div>Aboutus</div>;
+};
+
+export default AboutUs;
