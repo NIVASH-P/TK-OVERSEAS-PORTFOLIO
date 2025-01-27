1006475732ea1359f6cb68ca74f85d07b46c501f
G-Pavithran-dev <pavithranadithya1441@gmail.com>
2025-01-27 18:37:42 +0530

    work initiated

---
+++ vite-project/src/components/Home/HomePage.jsx
@@ -0,0 +1,14 @@
+const HomePage = () => {
+  /**
+   * @author Pavithran G
+   * @description This is the home page component for the application.
+   * @returns {JSX.Element} The HomePage component.
+   */
+
+  return (
+    <section className="home-page">
+        
+    </section>
+  )
+}
+export default HomePage
---
+++ vite-project/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+
+    /**
+     * @author Pavithran G
+     * @description This is the Navbar component for the application.
+     * @returns {JSX.Element} The Navbar component.
+     */
+  return (
+    <section className="navbar">
+
+    </section>
+  )
+}
+export default Navbar
