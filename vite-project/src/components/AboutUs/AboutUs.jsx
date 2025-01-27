3e666476eed2844ff3d555d46848645c15af284b
Nithishuchiha <124854933+Nithishuchiha@users.noreply.github.com>
2025-01-27 21:24:16 +0530

    about us updated

--- vite-project/src/components/AboutUs/AboutUs.jsx
+++ vite-project/src/components/AboutUs/AboutUs.jsx
@@ -1,7 +1,7 @@
 /**
  * @author Nithish_kumar_P
- * @description These page has a content oƒ about us page
- * @returns {JSX.ELEMENT} These page returns about us component
+ * @description These page has a content oƒ about me page
+ * @returns {JSX.ELEMENT} These page returns about me component
  */
 
 import "./AboutUs.css";
