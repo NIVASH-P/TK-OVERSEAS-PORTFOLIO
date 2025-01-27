db428a23038afe3635429514f15ce9783d92840d
NIVASH-P <nivashpns666@gmail.com>
2025-01-27 20:14:52 +0530

    Error Cleared

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,14 +2,18 @@ import React from "react"
 import { Routes, BrowserRouter as Router, Route } from "react-router-dom"
 import HomePage from "./components/Home/HomePage"
 import Navbar from "./components/Navbar/Navbar"
+import FeaturePage from "./components/Features/FeaturePage"
 
 function App() {
+  return(
     <Router>
         <Navbar />
         <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="/feature" element={<FeaturePage />}/>
         </Routes>
     </Router>
+  )
 }
 
 export default App
