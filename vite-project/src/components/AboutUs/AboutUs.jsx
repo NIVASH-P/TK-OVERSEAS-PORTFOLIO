2403ec7e0ff82d24d7646efc79750d0b7f55c939
Nithishuchiha <124854933+Nithishuchiha@users.noreply.github.com>
2025-01-29 11:56:42 +0530

    about-us page completed,MUI ICONS USED

--- vite-project/src/components/AboutUs/AboutUs.jsx
+++ vite-project/src/components/AboutUs/AboutUs.jsx
@@ -5,21 +5,55 @@
  */
 
 import "./AboutUs.css";
+import ArrowRightAltSharpIcon from "@mui/icons-material/ArrowRightAltSharp";
+
 const AboutUs = () => {
   return (
-    <div className="aboutus-container">
-      <div className="image">
-        <img src="../../../public/about-banner.png" alt="" />
-      </div>
-      <div className="aboutus-content">
-        <h2>ABOUT US</h2>
-        <h1 className="font-style">I AM AVAILABLE FOR UI UX DESIGN PROJECTS</h1>
-        <h3>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim quia
-          voluptatum hic deserunt, aliquid porro ab aliquam deleniti consectetur
-          reiciendis odio minus repellat nihil dolorem facilis ipsa beatae
-          magnam eius.
-        </h3>
+    <div className="aboutus">
+      <div className="aboutus-container aboutus-scoped">
+        <div className="image-card">
+          <div className="image">
+            <img src="/images/blured.png" alt="aboutUs-image" />
+          </div>
+        </div>
+        <div className="aboutus-content">
+          <h2 className="aboutus-heading">ABOUT US</h2>
+          <h1 className="aboutus-description">
+            I AM FOUNDER OF <span className="aboutus-job">TK OVERSEAS</span>{" "}
+          </h1>
+          <div className="aboutus-details">
+            FOUNDER & CEO OF TK OVERSEAS EDUCATION CONSULTANCY | CAREER
+            CONSULTANT FOR ABROAD STUDIES | EDUCATION LOAN | STUDY & WORK VISA
+            ASSISTANCE SERVICE | MEMBER OF JCI | CAREER COUNSELLING CERTIFIED
+            CAREER COUNSELLOR FOR INTERNATIONAL STUDIES
+          </div>
+          <div className="card-container">
+            <div className="card">
+              <h3>
+                <span className="card-number">270+</span>{" "}
+                <span className="card-text">Student Visa</span>
+              </h3>
+            </div>
+            <div className="card">
+              <h3>
+                <span className="card-number">6+</span>{" "}
+                <span className="card-text">Years Experience</span>
+              </h3>
+            </div>
+            <div className="card">
+              <h3>
+                <span className="card-number">40+</span>{" "}
+                <span className="card-text">Tourist Visa</span>
+              </h3>
+            </div>
+          </div>
+          <button className="aboutus-button">
+            GET IN TOUCH <ArrowRightAltSharpIcon />{" "}
+          </button>
+        </div>
+        <div className="aboutus-roller">
+          <img src="/effect-2.svg" width={300} />
+        </div>
       </div>
     </div>
   );
